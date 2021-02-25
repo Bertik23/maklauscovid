880a5d6175f0c14d163624db034a83ac6ab4a5cd
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2021-02-25 20:15:02 +0100

    gunicorn 2?

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask("maKlausCovid")
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run()
+if __name__ == "__main__":
+    app.run()
